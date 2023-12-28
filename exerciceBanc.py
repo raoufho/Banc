d28a19df2eb05a5d2f2d7da6ec477c8876328cfa
Raouf Malek <Raoufho05@gmail.com>
2023-12-28 13:09:57 +0100

    - Both the first and second menu messages - Several quotes between the words balance and name were replaced with a single quote. - The print of the account creation method was providing us with an incorrect value. - Floats, not str, are required for the amount variable in the withdrawal and deposit methods. - Option 3: useddeposit rather than display.

--- exerciceBanc.py
+++ exerciceBanc.py
@@ -12,14 +12,15 @@ class Bank:
     def __init__(self):
         self.accounts = {}
 
-    def creataccount(self, name, initial_deposit):
-        account_number = self.account_number_generator()
-        self.accounts[account_number] = {"name": name, "balance": initial_deposit}
-        print("your account number is: ", bank.account_number_generator())
-
 
     def account_number_generator(self):
         return str(random.randint(1,99999)).zfill(5)
+
+
+    def creataccount(self, name, initial_deposit):
+        account_number = self.account_number_generator()
+        self.accounts[account_number] = {'name': name, 'balance': initial_deposit}
+        print("your account number is: ", account_number)
         
 
     def check_valid_user(self, name, account_number):
@@ -29,65 +30,73 @@ class Bank:
             return False
 
         
-    def withdraw(self, initial_deposit):
-        amount = input()
-        initial_deposit -= amount
-        print("You have :",initial_deposit)
+    def withdraw(self, account_number, amount):
+        amount = float(amount)
+        self.accounts[account_number]['balance'] -= amount
+        print(f"Available balance: {self.accounts[account_number]['balance']}")
+        
 
-    def deposit(self, initial_deposit):
-        amount = input()
-        initial_deposit += amount
-        print('You have :',initial_deposit)
+    def deposit(self, account_number, amount):
+        amount = float(amount)
+        self.accounts[account_number]['balance'] += amount
+        print(f"Available balance: {self.accounts[account_number]['balance']}")
 
-    def display(self, initial_deposit):
-        print("You have: ", initial_deposit)
+    def display(self, account_number):
+        print("You have: ", self.accounts[account_number]['balance'])
 
 
 bank = Bank()
 while True:
-    print("Welcome!")
-    print("Press 1 to creat a new account")
-    print("Press 2 to access to an existing account")
-    print("Press 3 to exit")
+    print("\nWelcome to the Banking System!")
+    print("\n1 Creat a new account")
+    print("2 Access to an existing account")
+    print("3 Exit")
     
-    choice = input() 
+    choice = input("\nEnter the option number: ") 
     if choice == "1":
         name = input("Enter your name: ")
         initial_deposit = float(input("Enter your initial deposit amount: "))
         bank.creataccount(name, initial_deposit)
 
     elif choice == "2":
-        name = input("Enter you name: ")
+        name = input("Enter your name: ")
         account_number = input("Enter your account number: ")
-        bank.check_valid_user(name, account_number)
+        
+        if bank.check_valid_user(name, account_number):
 
-        while True:
-            print("1: Withdraw: ")
-            print("2: Deposit")
-            print("3: Display balance")
-            print("4: Return to the main menu")
+            while True:
+                print("\nWhat would you like to do?")
+                print("1: Withdraw: ")
+                print("2: Deposit")
+                print("3: Display balance")
+                print("4: Return to the main menu")
 
-            option = input(int())
+                option = input("Enter the option number: ")     #int input doesn't work with string option
 
-            if option == "1":
-                bank.withdraw()
+                if option == "1":
+                    amount = input("Enter the withdrawal amount: ")
+                    bank.withdraw(account_number, amount)
 
-            elif option == "2":
-                bank.deposit()
+                elif option == "2":
+                    amount = input("Enter the deposit amount: ")
+                    bank.deposit(account_number, amount)
 
-            elif option == "3":
-                bank.deposit()
+                elif option == "3":
+                    bank.display(account_number)
 
-            elif option == "4":
-                break
+                elif option == "4":
+                    print("Reterning to the main menu")
+                    break
 
-            else:
-                print("Invalid option. Please enter a valid option (1, 2, 3, or 4).")
-
+                else:
+                    print("Invalid option. Please enter a valid option (1, 2, 3, or 4).")
+            
+        else:
+            print("Please check your name and account number")
 
     elif choice == "3":
         print("thank you for using our service")
         exit()
 
     else:
-        print("Invalid option")
+        print("Invalid option, Please enter a valid option (1, 2, or 3)")
